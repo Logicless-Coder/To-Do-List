98227bff7b0aeceb51ff88ca21df986509059ec5
Logicless-Coder <abhirathore565@gmail.com>
2020-06-23 15:41:54 +0530

    Done the basic todo app. Added checked and delete task functionality. Almost finished. Only extra (if required) stuff shall be added.

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { Typography, TextField } from "@material-ui/core";
+import { Typography, TextField, Grid } from "@material-ui/core";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import ClearIcon from "@material-ui/icons/Clear";
 import "./ToDoList.css";
 
 class ToDoList extends Component {
@@ -25,7 +28,29 @@ class ToDoList extends Component {
 
   onSubmitInputText = () => {
     var todosClone = [...this.state.todos];
-    todosClone.push(this.state.inputText);
+    var newTask = {
+      title: this.state.inputText,
+      checked: false,
+    };
+    todosClone.push(newTask);
+    this.setState({
+      todos: todosClone,
+      inputText: "",
+    });
+  };
+
+  toggleChecked = (index) => {
+    var todosClone = [...this.state.todos];
+    todosClone[index].checked = !todosClone[index].checked;
+    this.setState({
+      todos: todosClone,
+      inputText: "",
+    });
+  };
+
+  clearTask = (index) => {
+    var todosClone = [...this.state.todos];
+    todosClone.splice(index, 1);
     this.setState({
       todos: todosClone,
       inputText: "",
@@ -35,7 +60,7 @@ class ToDoList extends Component {
   render() {
     return (
       <div>
-        <Typography variant="h1" color="secondary">
+        <Typography variant="h1" color="secondary" id="title">
           todos
         </Typography>
         <TextField
@@ -52,11 +77,44 @@ class ToDoList extends Component {
           }
         />
         <div>
-          {this.state.todos.map((todo) => (
-            <Typography variant="h6" color="text-primary">
-              {todo}
+          {this.state.todos.length != 0 ? (
+            this.state.todos.map((todo, index) => (
+              <div className="task">
+                <Grid container id={index}>
+                  <Grid item xs={1}>
+                    {todo.checked && (
+                      <CheckCircleIcon
+                        onClick={() => this.toggleChecked(index)}
+                      />
+                    )}
+                    {todo.checked == false && (
+                      <RadioButtonUncheckedIcon
+                        onClick={() => this.toggleChecked(index)}
+                      />
+                    )}
+                  </Grid>
+                  <Grid item xs={1}></Grid>
+                  <Grid
+                    item
+                    xs={8}
+                    className={`${todo.checked ? "checked" : ""}`}
+                  >
+                    <Typography variant="h5" color="text-primary" align="left">
+                      {todo.title}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={1}></Grid>
+                  <Grid item xs={1}>
+                    <ClearIcon onClick={() => this.clearTask(index)} />
+                  </Grid>
+                </Grid>
+              </div>
+            ))
+          ) : (
+            <Typography variant="h5" color="text-primary" id="catch">
+              Your tasks will be shown here.
             </Typography>
-          ))}
+          )}
         </div>
       </div>
     );
