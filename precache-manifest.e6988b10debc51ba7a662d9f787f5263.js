eab27b9bd5f49237c68159887ccd846e592509b6
Logicless-Coder <abhirathore565@gmail.com>
2020-06-23 15:50:37 +0530

    Updates

---
+++ precache-manifest.e6988b10debc51ba7a662d9f787f5263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6ff24e9a9021de8069803f31e57bf2",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "95c09179129ec9a4c753",
+    "url": "/To-Do-List/static/css/main.cb83d2ca.chunk.css"
+  },
+  {
+    "revision": "a809d8abad0cd40eca95",
+    "url": "/To-Do-List/static/js/2.a589f855.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/To-Do-List/static/js/2.a589f855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c09179129ec9a4c753",
+    "url": "/To-Do-List/static/js/main.3a3edb04.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
